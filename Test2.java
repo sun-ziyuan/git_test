7cc06441c5cca4a1f560f3bf411dd5e93d75592d
szy <sunziyuan710@gmail.com>
2024-02-24 20:57:47 +0800

    第三次提交

---
+++ Test2.java
@@ -0,0 +1,9 @@
+/**
+ * @author sunziyuan
+ * @date 2024/2/24 20:56
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test~V2.0 最终版！！");
+    }
+}
